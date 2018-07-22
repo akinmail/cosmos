ac2322dbfdc3de675e27595647e006d35bf812a3
Akindele Akinyemi <akbumper@gmail.com>
2018-07-22 06:30:20 +0100

    Added Tracking api by bill of laden

--- src/main/java/com/cosmos/hack/controller/SchedulerController.java
+++ src/main/java/com/cosmos/hack/controller/SchedulerController.java
@@ -2,11 +2,18 @@ package com.cosmos.hack.controller;
 
 import com.cosmos.hack.model.Shipment;
 import com.cosmos.hack.repository.ImportProcessRepository;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,4 +30,11 @@ public class SchedulerController {
                 .flatMap(elt -> elt.getShipment().stream().filter(elt2 -> elt2.isDocumentComplete() == false))
                 .collect(Collectors.toList());
     }
+
+    public static void checkElement(String name, Element elem) {
+        if (elem == null) {
+            throw new RuntimeException("Unable to find " + name);
+        }
+    }
+
 }
--- src/main/java/com/cosmos/hack/controller/document/BolController.java
+++ src/main/java/com/cosmos/hack/controller/document/BolController.java
@@ -3,14 +3,45 @@ package com.cosmos.hack.controller.document;
 import com.cosmos.hack.controller.AbstractController;
 import com.cosmos.hack.model.document.Bol;
 import com.cosmos.hack.repository.document.BolRepository;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/document/bol")
+@RequestMapping("/bol")
 public class BolController extends AbstractController<Bol,String> {
     @Autowired
     public BolController(BolRepository repo) {
         super(repo);
+    }
+
+    @GetMapping("/search/{bolid}")
+    public List<String> getBolLocation(@PathVariable String bolid) throws IOException {
+        String url = "https://wheremy.com/bill-of-lading-tracking/show-tracking-info/mscgva/"+ bolid;
+        Connection.Response resp = Jsoup.connect(url) //
+                .timeout(30000) //
+                .method(Connection.Method.GET) //
+                .execute();
+
+// * Find the form
+        Document responseDocument = resp.parse();
+
+// * Extract results (entity numbers in this sample code)
+        List<String> resultSet = new ArrayList<>();
+        for (Element entityNumber : responseDocument.select("item")) {
+            resultSet.add(entityNumber.text());
+        }
+        Collections.reverse(resultSet);
+        return resultSet;
     }
 }
