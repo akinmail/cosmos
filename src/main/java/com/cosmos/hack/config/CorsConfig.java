6deed16f389b3c6c063fd6fd12087c58e16ac140
Akindele Akinyemi <akbumper@gmail.com>
2018-07-22 01:27:18 +0100

    changes

---
+++ src/main/java/com/cosmos/hack/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.cosmos.hack.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/*")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .allowedHeaders("Content-Type")
+                .allowCredentials(false)
+                .maxAge(6000);
+
+    }
+}
